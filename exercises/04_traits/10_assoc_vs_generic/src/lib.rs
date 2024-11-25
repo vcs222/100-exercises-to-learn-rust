aea289f9bd5edb5e175289f48f9747868d0e1027
Csaba Veres <vcs222@gmail.com>
2024-11-25 15:20:28 +0100

    solve 04 traits; 10 accoc vs generic

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,31 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+pub trait Power<Rhs> {
+    fn power(&self, n: Rhs) -> Self;
+}
+
+impl Power<u16> for u32 {
+    fn power(&self, n: u16) -> Self {
+        self.pow(n as u32)
+    }
+}
+impl Power<u32> for u32 {
+    fn power(&self, n: u32) -> Self {
+        self.pow(n)
+    }
+}
+impl Power<&u16> for u32 {
+    fn power(&self, n: &u16) -> Self {
+        self.pow(*n as u32)
+    }
+}
+impl Power<&u32> for u32 {
+    fn power(&self, n: &u32) -> Self {
+        self.pow(*n)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
