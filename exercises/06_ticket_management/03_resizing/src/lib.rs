c42fc0f19ddcd0b9df404aadc9650af2708ab345
Csaba Veres <vcs222@gmail.com>
2024-11-28 15:50:09 +0100

    solve 06 ticket management; 03 resizing

--- exercises/06_ticket_management/03_resizing/src/lib.rs
+++ exercises/06_ticket_management/03_resizing/src/lib.rs
@@ -12,6 +12,11 @@ mod tests {
         // Can you guess what the new capacity will be?
         // Beware that the standard library makes no guarantees about the
         // algorithm used to resize the vector, so this may change in the future.
-        assert_eq!(v.capacity(), todo!());
+        assert_eq!(v.capacity(), 4);
+
+        v.push(4);
+        v.push(5); // beyond capacity, needs to resize
+
+        assert_eq!(v.capacity(), 8);
     }
 }
