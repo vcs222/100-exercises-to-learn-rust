1b6fad81a099a63faeeebba705cd359f0472277d
Csaba Veres <vcs222@gmail.com>
2024-12-01 10:26:03 +0100

    solve 07 threads; 01 threads

--- exercises/07_threads/01_threads/src/lib.rs
+++ exercises/07_threads/01_threads/src/lib.rs
@@ -15,7 +15,14 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+    let left: Vec<i32> = v[..mid].to_vec();
+    let right: Vec<i32> = v[mid..].to_vec();
+
+    let left_handle = thread::spawn(move || left.iter().sum::<i32>());
+    let right_handle = thread::spawn(move || right.iter().sum::<i32>());
+
+    left_handle.join().unwrap() + right_handle.join().unwrap()
 }
 
 #[cfg(test)]
