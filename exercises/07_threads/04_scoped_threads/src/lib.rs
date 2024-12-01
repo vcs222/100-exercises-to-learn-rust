589ce8b75ed353283644a2882874bf0e185ff25e
Csaba Veres <vcs222@gmail.com>
2024-12-01 10:58:55 +0100

    solve 07 threads; 04 scoped threads

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -3,7 +3,17 @@
 //  Don't perform any heap allocation. Don't leak any memory.
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+
+    std::thread::scope(|scope| {
+        let left = &v[..mid];
+        let right = &v[mid..];
+
+        let left_handle = scope.spawn(|| left.iter().sum::<i32>());
+        let right_handle = scope.spawn(|| right.iter().sum::<i32>());
+
+        left_handle.join().unwrap() + right_handle.join().unwrap()
+    })
 }
 
 #[cfg(test)]
