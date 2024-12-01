7eac5dbfdf79adf7cf039af9fb2056e436fd80c6
Csaba Veres <vcs222@gmail.com>
2024-12-01 10:45:36 +0100

    solve 07 threads; 03 leak on purpose

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,16 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let v = v.into_boxed_slice();
+    let v = Box::leak(v);
+    let mid = v.len() / 2;
+    let left = &v[..mid];
+    let right = &v[mid..];
+
+    let left_handle = thread::spawn(move || left.iter().sum::<i32>());
+    let right_handle = thread::spawn(move || right.iter().sum::<i32>());
+
+    left_handle.join().unwrap() + right_handle.join().unwrap()
 }
 
 #[cfg(test)]
